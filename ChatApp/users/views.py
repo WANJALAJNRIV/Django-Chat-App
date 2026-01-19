81c24c7eae2621ef4d8227da14bde16051b55a55
WANJALAJNRIV <wanjalajnr04@gmail.com>
2026-01-19 21:06:40 +0300

    Set up the application. Added the home and authentication apps

---
+++ ChatApp/users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import SignUpForm
+from django.contrib.auth.decorators  import login_required
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after signup
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
